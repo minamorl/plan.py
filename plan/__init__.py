53f2e0d674f57b564fe2f0e108a52de183494e02
minamorl <minamorl@users.noreply.github.com>
2015-11-14 22:36:45 +0900

    add asynchronous option

--- plan/__init__.py
+++ plan/__init__.py
@@ -2,9 +2,11 @@ import subprocess
 import os
 
 
-def shell(s, on=None, start_new_session=False):
+def shell(s, on=None, start_new_session=False, asynchronous=True):
 
     if on is not None:
         on = os.path.expanduser(on)
 
-    return subprocess.Popen(s, cwd=on, start_new_session=start_new_session)
+    if asynchronous:
+        return subprocess.Popen(s, cwd=on, start_new_session=start_new_session)
+    return subprocess.run(s, cwd=on, start_new_session=start_new_session)
