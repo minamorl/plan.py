113e90e951190dee953149ef72bb10aef07ae6bc
minamorl <minamorl@users.noreply.github.com>
2015-11-15 12:51:12 +0900

    add run for running a simple task

--- plan/__init__.py
+++ plan/__init__.py
@@ -8,3 +8,7 @@ def shell(s, on=None, start_new_session=False, asynchronous=True):
     if asynchronous:
         return subprocess.Popen(s, cwd=on, start_new_session=start_new_session)
     return subprocess.run(s, cwd=on, start_new_session=start_new_session)
+
+
+def run(s, on=None, start_new_session=False):
+    return shell(s, on=on, start_new_session=start_new_session, asynchronous=False)
